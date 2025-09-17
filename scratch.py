72e7eac58fa43e949088c45baed8676a20dea1e7
kavunarbuz <g.a.studios36@gmail.com>
2025-09-17 09:25:33 +0300

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,37 @@
+#1.1
+print(31,18,79)
+
+#1.2
+print(47,52,150,sep= '  ')
+
+#1.3
+print(50)
+print(10)
+
+#1.4
+print(5)
+print(10)
+print(21)
+
+#1.5
+print(1)
+print(2)
+
+#1.6
+import math
+print(f"{math.pi:.3f}")
+
+#1.7
+import math
+print(f"{math.e:.1f}")
+
+#1.8
+num=input("Введите число:")
+print("Вы вывели число",num)
+
+#1.9
+num =input ("число:")
+print(num,"вот такое число Вы ввели")
+
+#1.10
+name=input("введите имя")
