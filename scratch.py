b198012112ecdf14c66edff7024ab185dfd6e2e3
kavunarbuz <g.a.studios36@gmail.com>
2025-09-24 08:40:33 +0300

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,37 +1,48 @@
-#1.1
-print(31,18,79)
-
-#1.2
-print(47,52,150,sep= '  ')
-
-#1.3
-print(50)
-print(10)
-
-#1.4
-print(5)
-print(10)
-print(21)
-
-#1.5
-print(1)
-print(2)
-
-#1.6
 import math
-print(f"{math.pi:.3f}")
 
-#1.7
-import math
-print(f"{math.e:.1f}")
+def main():
+    while True:
+        print("Введите номер программы ")
+        select_program = input()
 
-#1.8
-num=input("Введите число:")
-print("Вы вывели число",num)
+        if select_program == "1.1":
+            print(31,18,79)
 
-#1.9
-num =input ("число:")
-print(num,"вот такое число Вы ввели")
+        elif select_program == "1.2":
+            print(47,52,150,sep= '  ')
 
-#1.10
+        elif select_program == "1.3":
+            print(50)
+            print(10)
+
+        elif select_program == "1.4":
+            print(5)
+            print(10)
+            print(21)
+
+        elif select_program == "1.5":
+            print(1)
+            print(2)
+
+        elif select_program == "1.6":
+            print(f"{math.pi:.3f}")
+
+        elif select_program == "1.7":
+            print(f"{math.e:.1f}")
+
+        elif select_program == "1.8":
+            num=input("Введите число:")
+            print("Вы вывели число",num)
+
+        elif select_program == "1.9":
+            num =input ("число:")
+            print(num,"вот такое число Вы ввели")
+        
+        else:
+            print("вы ввели не верное число")
+
+if __name__ == "__main__":
+    main()
+
+elif select_program == "1.10":
 name=input("введите имя")
